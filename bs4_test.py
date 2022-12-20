3f6aa011a8792f4f2b8eac79f698d2e24f9136e3
seungm2n <seungm2n2@gmail.com>
2022-12-20 13:08:56 +0900

    Web Crwaling Test 추가

--- bs4_test.py
+++ bs4_test.py
@@ -1,4 +1,9 @@
-from bs import BeautifulSoup
+from bs4 import BeautifulSoup
 
 html = open("fruits.html", "r", encoding="utf-8")
-BeautifulSoup(html, "html.parse")
+soup = BeautifulSoup(html, "html.parse")
+body = soup.select_one("body")             # 객체.select_one(<선택자>) : CSS 선택자로 요소 하나를 추출함.
+p = body.find('p')                             # 객체.find(tag[, attributes]) : tag라는 태그 중 조건에 맞는 1번째 태그를 찾아줌
+
+print('1번째 p태그 : ', ptag)
+print('1번째 p태그 : ', ptag['class'])
